7a2001059744309cec7e1b34ec23ec1cf753a346
jakobi007 <sylwia.salapa@gmail.com>
2023-05-11 12:22:11 +0100

    showing current weather conditions

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
+function displayTemp(response) {
+  let tempElement = document.querySelector("#temperature");
+  tempElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = response.data.time * 1000;
+}
+let key = "2374eba8044tffoa6bba2f4241b376c8";
+let url =
+  "https://api.shecodes.io/weather/v1/current?query=New York&key=2374eba8044tffoa6bba2f4241b376c8&units=metric";
 
+axios.get(url).then(displayTemp);
