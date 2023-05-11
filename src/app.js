10e2595208e28f71b426a3edff12795b61bd5eee
jakobi007 <sylwia.salapa@gmail.com>
2023-05-11 12:44:59 +0100

    updated time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatDate(timeStamp) {
+  let date = new Date(timeStamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours} ${minutes}`;
+}
+
 function displayTemp(response) {
   let tempElement = document.querySelector("#temperature");
   tempElement.innerHTML = Math.round(response.data.temperature.current);
@@ -10,7 +33,7 @@ function displayTemp(response) {
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let dateElement = document.querySelector("#date");
-  dateElement.innerHTML = response.data.time * 1000;
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
 }
 let key = "2374eba8044tffoa6bba2f4241b376c8";
 let url =
